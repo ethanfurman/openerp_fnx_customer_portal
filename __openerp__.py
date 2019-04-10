ae672044f282acab9ecadc78383e51644aef080f
Ethan Furman <ethan@stoneleaf.us>
2019-04-09 22:37:48 -0700

    add dependencies

--- __openerp__.py
+++ __openerp__.py
@@ -4,22 +4,25 @@
     'category': 'Generic Modules',
     'description': """\
             Phoenix Portal for customers.
-	    
-	    Allows entering orders on-line instead of through hand-held
-	    devices.
+
+        Allows entering orders on-line instead of through hand-held
+        devices.
             """,
     'author': 'Ethan Furman',
     'maintainer': 'Ethan Furman',
     'website': '',
     'depends': [
+        'base',
+        'fis_integration',
         'mail',
+        'web',
         ],
     'js': [
         ],
     'css':[
         ],
     'data': [
-	    'order_entry_view.xaml',
+        'order_entry_view.xaml',
         ],
     'test': [],
     'installable': True,
