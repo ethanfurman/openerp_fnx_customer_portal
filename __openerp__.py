1c540cf4344c4b64ec0b2cf10cd01fc7c9cfedfa
Ethan Furman <ethan@stoneleaf.us>
2019-04-09 21:28:56 -0700

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Fnx Customer Portal',
+    'version': '0.1',
+    'category': 'Generic Modules',
+    'description': """\
+            Phoenix Portal for customers.
+	    
+	    Allows entering orders on-line instead of through hand-held
+	    devices.
+            """,
+    'author': 'Ethan Furman',
+    'maintainer': 'Ethan Furman',
+    'website': '',
+    'depends': [
+        'mail',
+        ],
+    'js': [
+        ],
+    'css':[
+        ],
+    'data': [
+	    'order_entry_view.xaml',
+        ],
+    'test': [],
+    'installable': True,
+    'active': False,
+}
+
